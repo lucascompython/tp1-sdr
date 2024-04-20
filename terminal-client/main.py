83af4a3818f57361f47aee029ac31deca14a47e4
Lucas de Linhares <lucasdelinhares@gmail.com>
2024-04-20 21:12:28 +0100

    Make the terminal client prettier. The input is now always at the bottom

--- terminal-client/main.py
+++ terminal-client/main.py
@@ -3,6 +3,8 @@ import socket
 import sys
 import threading
 from pathlib import Path
+import os
+from collections import deque
 
 sys.path.append(str(Path(__file__).resolve().parent.parent))
 from shared import MessagePacket, PacketType
@@ -10,7 +12,16 @@ from shared import MessagePacket, PacketType
 HOST, PORT = "localhost", 12345
 
 
-def receive_messages(sock: socket.socket) -> None:
+def clear_screen() -> None:
+    if os.name == "nt":
+        os.system("cls")
+    else:
+        os.system("clear")
+
+
+def receive_messages(
+    sock: socket.socket, messages: deque, username: str, term_size: os.terminal_size
+) -> None:
     while True:
         message = sock.recv(1024)
 
@@ -18,31 +29,55 @@ def receive_messages(sock: socket.socket) -> None:
 
         match json_decoded["type"]:
             case PacketType.MESSAGE.value:
-                print(f"{json_decoded['username']}: {json_decoded['message']}")
+                messages.append(
+                    f"{json_decoded['username']}: {json_decoded['message']}"
+                )
             case PacketType.LEAVE.value:
-                print(f"{json_decoded['username']} has left the chat.")
+                messages.append(f"{json_decoded['username']} has left the chat.")
             case PacketType.JOIN.value:
-                print(f"{json_decoded['username']} has joined the chat.")
+                messages.append(f"{json_decoded['username']} has joined the chat.")
             case _:
-                print(json_decoded)
+                messages.append(json_decoded)
+
+        clear_screen()
+        padding = term_size.lines - len(messages) - 1
+        print("\n".join(messages) + "\n" * padding + f"\n{username}> ", end="")
 
 
 def main() -> None:
+
+    clear_screen()
+
+    messages = deque(maxlen=256)
+
+    term_size = os.get_terminal_size()
+
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         sock.connect((HOST, PORT))
 
+        username = input("Enter your username: ")
+
         receive_thread = threading.Thread(
-            target=receive_messages, args=(sock,), daemon=True
+            target=receive_messages,
+            args=(sock, messages, username, term_size),
+            daemon=True,
         )
         receive_thread.start()
 
-        username = input("Enter your username: ")
+        clear_screen()
 
         sock.sendall(username.encode())
 
         try:
             while True:
-                message = input("> ")
+                padding = term_size.lines - len(messages) - 1
+                padding = "\n" * padding
+
+                print("\n".join(messages))
+
+                message = input(f"{padding + username}> ")
+                messages.append(f"{username}: {message}")
+
                 packet: MessagePacket = {
                     "type": PacketType.MESSAGE.value,
                     "username": username,
@@ -51,7 +86,7 @@ def main() -> None:
 
                 sock.sendall(json.dumps(packet).encode())
 
-                if message.lower() == "/exit":
+                if message.lower() in ("/exit", "/quit", "/q", "/e"):
                     break
         except KeyboardInterrupt:
             print("\nClosing connection...")
